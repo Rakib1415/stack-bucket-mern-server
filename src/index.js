22aeb573a4553b4e132d822622360ecbdde58b48
rakib1415 <rakibiuaece@gmail.com>
2021-01-06 09:51:53 +0600

    scaffold application completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
@@ -5,12 +7,38 @@ const morgan = require('morgan');
 
 const app = express();
 
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname,'../','public')));
+app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.status(200).json({
         msg:'Hello world'
     })
+});
+
+app.use((req, res, next)=> {
+    const error = new Error('404 page not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next)=> {
+    console.log(error);
+    if(error.status === 404){
+        return res.status(404).json({
+            msg : error.message,
+            status : 404
+        })
+    }
+    return res.status(500).json({
+        msg : 'Internal Server Error',
+        status : 500
+    })
 })
 
-app.listen(8080, ()=> {
-    console.log('Listening on port 8080');
+app.listen(process.env.PORT, ()=> {
+    console.log('Listening on port ', process.env.PORT);
 })
